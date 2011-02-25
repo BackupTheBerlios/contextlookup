7a524c6a61978cb615686657d031468f83e69067
diego_ogeid <diego_ogeid>
2011-02-25 23:55:20 +0000

    Refactored the UI and the code for simplicity

--- ContextLookup/src/com/diegor/contextLookup/ChoseContactScreen.java
+++ ContextLookup/src/com/diegor/contextLookup/ChoseContactScreen.java
@@ -18,6 +18,7 @@
 
 package com.diegor.contextLookup;
 
+import net.rim.blackberry.api.browser.Browser;
 import net.rim.blackberry.api.invoke.Invoke;
 import net.rim.blackberry.api.mail.Message;
 import net.rim.blackberry.api.mail.MessagingException;
@@ -25,23 +26,20 @@ import net.rim.device.api.ui.Field;
 import net.rim.device.api.ui.FieldChangeListener;
 import net.rim.device.api.ui.MenuItem;
 import net.rim.device.api.ui.UiApplication;
-import net.rim.device.api.ui.XYEdges;
 import net.rim.device.api.ui.component.ButtonField;
-import net.rim.device.api.ui.component.EditField;
 import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.component.Menu;
 import net.rim.device.api.ui.component.ObjectChoiceField;
 import net.rim.device.api.ui.component.RichTextField;
+import net.rim.device.api.ui.component.SeparatorField;
 import net.rim.device.api.ui.container.MainScreen;
-import net.rim.device.api.ui.decor.BorderFactory;
-import net.rim.device.api.util.Arrays;
 
 final class ChooseContactScreen extends MainScreen implements
 		ObserverInterface, FieldChangeListener {
 	// This screen should be displayed after the menu item "Lookup Contact" is
 	// selected. It is the only visible screen of this app. I would like to
 	// rewrite it so that it looks like the BB Addressbook.
-	private EditField user_input;
+	private ButtonField user_input;
 	// to be able to use the "for" cycle
 	int[] recipientTypeIterator = { // TODO change to final
 	Message.RecipientType.FROM, Message.RecipientType.TO,
@@ -62,7 +60,7 @@ final class ChooseContactScreen extends MainScreen implements
 		public void run() {
 			RemoteLookup lookup_email = new RemoteLookup();
 			Invoke.invokeApplication(Invoke.APP_TYPE_ADDRESSBOOK, null);
-			lookup_email.doLookup(user_input.getText());
+			lookup_email.doLookup(user_input.getLabel());
 			onClose();
 		}
 	};
@@ -70,7 +68,7 @@ final class ChooseContactScreen extends MainScreen implements
 			"Lookup Contact on LinkedIn", 100, 10) {
 		public void run() {
 			RemoteLookup lookup_email = new RemoteLookup();
-			lookup_email.doLinkedInLookup(user_input.getText());
+			lookup_email.doLinkedInLookup(user_input.getLabel());
 			onClose();
 		}
 	};
@@ -78,7 +76,7 @@ final class ChooseContactScreen extends MainScreen implements
 			"Lookup Contact on 123people", 100, 10) {
 		public void run() {
 			RemoteLookup lookup_email = new RemoteLookup();
-			lookup_email.do123peopleLookup(user_input.getText());
+			lookup_email.do123peopleLookup( user_input.getLabel());
 			onClose();
 		}
 
@@ -104,34 +102,39 @@ final class ChooseContactScreen extends MainScreen implements
 		// the "from:" address field should always be
 		// available so we use it as a default
 		try {
-			from_address = message.getFrom().getAddr();
+			from_address = message.getFrom().getName();
+			from_address =(from_address instanceof String)?from_address:message.getFrom().getAddr();
 		} catch (Exception ex) {
-			from_address = "";
+			from_address = "Choose A Contact Below";
 		}
-		user_input = new EditField("Lookup: ", from_address);
-		// TODO substitute with the pop-up text field like in the vanilla lookup
-		// from the address book.
-		user_input.setBorder(BorderFactory.createRoundedBorder(new XYEdges(10,
-				10, 10, 10)));
-		this.add(user_input);
+		user_input = new ButtonField( from_address);
+		LabelField lfl=new LabelField( "Lookup:");
+		JustifiedHorizontalFieldManager hml = new JustifiedHorizontalFieldManager(
+				lfl, user_input, true);
+		// Had to create a custom Field Manager as the vanilla one
+		// does not support justified layout.
+		this.add(hml);
+		SeparatorField sf = new SeparatorField(SeparatorField.LINE_HORIZONTAL);
+		this.add(sf);
 
 		// Doing this in two steps to prepare for the multithreading and MVC
 		// paradigm. First we collect all Addresses and Names from the message.
-		String[/* field_type */][/* address_or_name */][/* members */] addressList = new String[recipientTypeListLength][2][/* different_for_al_fields */];
+		String[/* field_type */][/* members */] addressList = new String[recipientTypeListLength][/* different_for_al_fields */];
 		for (int i = recipientTypeListLength - 1; i >= 0; i--) {
 			// this iteration style should speed up a little
 			try {
 				int numberOfRecipients = message
 						.getRecipients(recipientTypeIterator[i]).length;
 				if (numberOfRecipients > 0) {
-					addressList[i] = new String[2][numberOfRecipients];
+					addressList[i] = new String[numberOfRecipients];
 					for (int j = numberOfRecipients - 1; j >= 0; j--) {
-						addressList[i][0][j] = message
-								.getRecipients(recipientTypeIterator[i])[j]
-								.getAddr();
-						addressList[i][1][j] = message
+						addressList[i][j] = message
 								.getRecipients(recipientTypeIterator[i])[j]
 								.getName();
+						addressList[i][j] = (addressList[i][j] instanceof String) ? addressList[i][j]
+								: message
+										.getRecipients(recipientTypeIterator[i])[j]
+										.getAddr();
 					}
 				}
 			} catch (MessagingException e) {
@@ -147,59 +150,48 @@ final class ChooseContactScreen extends MainScreen implements
 
 		// From here on we start adding fields filled with the collected
 		// information.
-		add(new RichTextField("Chose from Contact Address(es):"));
+		add(new RichTextField("Choose Another Contact To Look Up:"));
 
-		for (int i = 0; i < recipientTypeListLength; i++) {
-			if (addressList[i][0] instanceof String[]) { // not null
-				if (addressList[i][0].length == 1) { // add a button instead of
-					// a drop down list
-					ButtonField bf = new ButtonField(addressList[i][0][0],
+		for (int rt = 0; rt < recipientTypeListLength; rt++) {
+			if (addressList[rt] instanceof String[]) { // email not null
+				if (addressList[rt].length == 1) {
+					// add a button instead of a drop down list
+					ButtonField bf = new ButtonField(addressList[rt][0],
 							ButtonField.CONSUME_CLICK);
-					LabelField lf = new LabelField(fieldNameIterator[i]);
+					LabelField lf = new LabelField(fieldNameIterator[rt]);
 					JustifiedHorizontalFieldManager hm = new JustifiedHorizontalFieldManager(
 							lf, bf, true);
-					// Had to create a custom Field Manager as the vanilla does
-					// not support justified layout.
+					// Had to create a custom Field Manager as the vanilla one
+					// does not support justified layout.
 					bf.setChangeListener(this);
 					this.add(hm);
 				} else { // dropdown list
 					ClickableObjectChoiceField ocf;
 					this.add(ocf = new ClickableObjectChoiceField(
-							fieldNameIterator[i], addressList[i][0]));
+							fieldNameIterator[rt], addressList[rt]));
 					ocf.setChangeListener(this);
 				}
 			}
 			;
 		}
-
-		add(new RichTextField("Chose from Contact Name(s)"));
-
-		for (int i = 0; i < recipientTypeListLength; i++) {
-			if (addressList[i][1] instanceof String[]) { // not null
-				for (int k = (addressList[i][1].length - 1); k >= 0; k--) {
-					// remove null members, implicitly using the reverse cycle
-					// order
-					if (!(addressList[i][1][k] instanceof String))
-						Arrays.removeAt(addressList[i][1], k);
-				}
-				if (addressList[i][1].length == 1) {
-					// add a button instead of a drop down list
-					ButtonField bf = new ButtonField(addressList[i][1][0],
-							ButtonField.CONSUME_CLICK);
-					LabelField lf = new LabelField(fieldNameIterator[i]);
-					JustifiedHorizontalFieldManager hm = new JustifiedHorizontalFieldManager(
-							lf, bf, true);
-					bf.setChangeListener(this);
-					this.add(hm);
-				} else { // dropdown list
-					ClickableObjectChoiceField ocf;
-					this.add(ocf = new ClickableObjectChoiceField(
-							fieldNameIterator[i], addressList[i][1]));
-					ocf.setChangeListener(this);
+		SeparatorField sf2 = new SeparatorField(SeparatorField.LINE_HORIZONTAL);
+		this.add(sf2);
+/*		ButtonField feedback = new ButtonField("Feedback and License",
+				ButtonField.CONSUME_CLICK | ButtonField.FIELD_HCENTER
+						| ButtonField.HCENTER);
+		feedback.setChangeListener(new FieldChangeListener() {
+			public void fieldChanged(Field field, int context) {
+				if (field instanceof ButtonField) {
+					Browser
+							.getDefaultSession()
+							.displayPage(
+									"http://developer.berlios.de/forum/forum.php?forum_id=36545");
 				}
 			}
-		}
-
+		});
+		this.add(feedback);*/
+		add(new RichTextField("Copyright 2011 Egor Kobylkin, GPL3"));
+		
 		UiApplication.getUiApplication().pushScreen(this);
 	}
 
@@ -215,7 +207,7 @@ final class ChooseContactScreen extends MainScreen implements
 			// assumption: all dropdown lists contain email addresses or
 			// contacts
 			try {
-				user_input.setText((String) ((ObjectChoiceField) field)
+				user_input.setLabel((String) ((ObjectChoiceField) field)
 						.getChoice(((ObjectChoiceField) field)
 								.getSelectedIndex()));
 			} catch (Exception ex) {
@@ -224,7 +216,7 @@ final class ChooseContactScreen extends MainScreen implements
 		} else if (field instanceof ButtonField) {
 			try {
 				ButtonField buttonField = (ButtonField) field;
-				user_input.setText(buttonField.getLabel());
+				user_input.setLabel(buttonField.getLabel());
 			} catch (Exception ex) {
 				System.out.print(ex.toString());
 			}
